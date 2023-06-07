45aa51d7e94b91808f2b7856a94eff88e8d7d201
sat-ok <135455313+sat-ok@users.noreply.github.com>
2023-06-07 18:36:56 +0000

    Initial commit

---
+++ index.ts
@@ -0,0 +1,14 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//###########x
+let num1, num2, result: number;
+num1 = parseInt(prompt('entrer le numero 1'));
+num2 = parseInt(prompt('entrer le numero 2'));
+result = num1 * num2;
+if (result > 0) {
+  console.log('POSITIF');
+} else {
+  console.log('NEGATIF');
+}
+console.log('FIN');
