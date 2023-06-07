44ee97f3215ac315d2e8a2e2d14a3066e3ba3a66
sat-ok <135455313+sat-ok@users.noreply.github.com>
2023-06-07 18:42:56 +0000

    1 file modified

--- index.ts
+++ index.ts
@@ -6,7 +6,9 @@ let num1, num2, result: number;
 num1 = parseInt(prompt('entrer le numero 1'));
 num2 = parseInt(prompt('entrer le numero 2'));
 result = num1 * num2;
-if (result > 0) {
+if (result == 0) {
+  console.log('nul');
+} else if (result > 0) {
   console.log('POSITIF');
 } else {
   console.log('NEGATIF');
